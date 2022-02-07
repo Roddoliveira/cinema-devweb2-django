5725175bace9dadbfde2356e53aa168d0d5b1073
Roddoliveira <49770176+Roddoliveira@users.noreply.github.com>
2022-02-06 23:12:41 -0300

    Update urls.py

--- cinema/urls.py
+++ cinema/urls.py
@@ -35,5 +35,5 @@ urlpatterns = [
     path("categorias-apiview/<int:id>/", views.CategoriaDetail.as_view()),
     path("categorias-generic/", views.CategoriasListGeneric.as_view()),
     path("categorias-generic/<int:id>/", views.CategoriaDetailGeneric.as_view()),
-    path("api/", include(router.urls)),
+    path("", include(router.urls)),
 ]
